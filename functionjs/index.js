b4d6528360a6adc5c50b1200d03ccf70ca68097d
Ranjini29 <ranjiece@gmail.com>
2022-12-15 21:28:34 +0530

    intial change

---
+++ functionjs/index.js
@@ -0,0 +1,92 @@
+// let x = 2;
+// a()
+// b()
+// console.log(x);
+// function a(){
+//     let x =10;
+//     console.log(x);
+// }
+// function b(){
+//     let x =100;
+//     console.log(x);
+//}
+//  var x = 100;
+// function square(num){
+//     let ans = num * num;
+//     return ans;
+
+//  }
+// var ans1 =square(x);
+//  var ans2 =square (4);
+//   console.log(ans1,ans2);
+
+
+
+// function calofbill(){
+// const total = 100 *1.13;
+// console.log(`the value of ${total}`);
+//return total;
+// }
+// calofbill();
+
+// const billamount =100;
+// const tax =0.13;
+// function calofbill(){
+//     console.log("Running of the bill amount");
+//     const total =billamount*+(1+tax);
+//     return total;
+// }
+// const total1 = calofbill();
+// console.log(total1)
+
+// function calofbill(billamount,tax){
+//     console.log("Running of the bill amount");
+//     const total = billamount*+(1+tax);
+//     return total;
+// }
+// const total1 = calofbill(100,0.13);
+// const total2 = calofbill(200,0.15)
+// console.log(total1,total2);
+//let age = prompt('How old are you?', 100);
+
+//alert(`You are ${age} years old!`); // You are 100 years old! = prompt('How old are you?', 100)
+
+// const num = 2;
+// const num1 = 50;
+// function get(){
+//     const total = `The sum of two numbers ${num+num1}`;
+//     return total;
+// }
+// get();
+// console.log(get());
+//const number = prompt("enter the square number: ");
+//const total = Math.sqrt(25);
+//console.log(`total square root of number is ${total}`);
+//console.warn("oh no")
+//const object ={'fname':'ranjini', 'age':35}
+//console.log(object.fname);
+//object.fname = "pranav";
+//console.table(object);
+//let a = new Date;
+//console.log(a);
+//let a = "25";
+//let b = 10;
+//console.log(a+b);
+//a = Number(a);
+//console.log(a+b);
+//let c=Math.floor(5.8);
+//c = Math.floor(Math.random()*5);
+//console.log(c);
+//let a =30;
+//if(a>=35 && a<=100){
+ //console.log("The person is pass");
+//}else{console.log("The person is fail")}
+a = " I am from kanchipuram";
+console.log("before replace:" +a);
+c =a.replace("kanchipuram","cheyyar")
+console.log("after replace:" +c);
+// include
+motor=["car","bike","cycle"];
+console.log(motor.includes("aero"));
+
+
